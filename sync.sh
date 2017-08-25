e92ad45720cafbbb7c7a2ec9440d514f2b54e3df
tangloner <tangloner@gmail.com>
2017-08-25 21:29:43 +0800

    sync

---
+++ sync.sh
@@ -0,0 +1,57 @@
+#!bin/bash
+echo "1"
+./ps_2.sh 1 1 sync
+./ps_2.sh 1 2 sync
+./ps_2.sh 1 3 sync
+./ps_2.sh 1 4 sync
+./ps_2.sh 1 5 sync
+./ps_2.sh 1 6 sync
+./ps_2.sh 1 7 sync
+./ps_2.sh 1 8 sync
+./ps_2.sh 1 9 sync
+echo "2"
+./ps_2.sh 2 1 sync
+./ps_2.sh 2 2 sync
+./ps_2.sh 2 3 sync
+./ps_2.sh 2 4 sync
+./ps_2.sh 2 5 sync
+./ps_2.sh 2 6 sync
+./ps_2.sh 2 7 sync
+./ps_2.sh 2 8 sync
+echo "3"
+./ps_2.sh 3 1 sync
+./ps_2.sh 3 2 sync
+./ps_2.sh 3 3 sync
+./ps_2.sh 3 4 sync
+./ps_2.sh 3 5 sync
+./ps_2.sh 3 6 sync
+./ps_2.sh 3 7 sync
+echo "4"
+./ps_2.sh 4 1 sync
+./ps_2.sh 4 2 sync
+./ps_2.sh 4 3 sync
+./ps_2.sh 4 4 sync
+./ps_2.sh 4 5 sync
+./ps_2.sh 4 6 sync
+echo "5"
+./ps_2.sh 5 1 sync
+./ps_2.sh 5 2 sync
+./ps_2.sh 5 3 sync
+./ps_2.sh 5 4 sync
+./ps_2.sh 5 5 sync
+echo "6"
+./ps_2.sh 6 1 sync
+./ps_2.sh 6 2 sync
+./ps_2.sh 6 3 sync
+./ps_2.sh 6 4 sync
+echo "7"
+./ps_2.sh 7 1 sync
+./ps_2.sh 7 2 sync
+./ps_2.sh 7 3 sync
+echo "8"
+./ps_2.sh 8 1 sync
+./ps_2.sh 8 2 sync
+echo "9"
+./ps_2.sh 9 1 sync
+
+
